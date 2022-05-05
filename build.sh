cc55d9861bf557942800e06f79353cc7132e7e19
Sevim Cengiz <sevim_cengiz@icloud.com>
2022-05-05 16:25:45 +0400

    Use build.sh for rendering Rmd files

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 set -ev
 
-export RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/quarto/bin"
+# Export environment variable only on macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/quarto/bin"
+fi
 cd mrs-educational-book
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', output_dir = '../book')"
 cd ..
-git push
